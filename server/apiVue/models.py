09c0c8bbcb242b90cec4ac4e88881e758adbbced
ledoa <ledoagael@gmail.com>
2025-04-30 12:48:39 +0300

    Test de mise en place django

--- server/apiVue/models.py
+++ server/apiVue/models.py
@@ -1,3 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
